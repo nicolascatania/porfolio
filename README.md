564988296f37f13223e1a8a7bfd96dfde40905b3
nicolascatania <nicolas20032401@gmail.com>
2025-02-04 16:35:51 -0300

    add hero component

--- README.md
+++
@@ -1,2 +0,0 @@
-# porfolio
-Personal porfolio to showcase my projects and abilities, and practice frontend with tailwind.
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HeroComponent } from "./hero/hero.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NavBarComponent, HeroComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'porfolio';
+}
