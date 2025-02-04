bfb8959aef46cfd3d8d93963d586851399448b3b
nicolascatania <nicolas20032401@gmail.com>
2025-02-04 19:53:26 -0300

    add projects component and changed some small details on hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HeroComponent } from "./hero/hero.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, HeroComponent],
+  imports: [NavBarComponent, HeroComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
