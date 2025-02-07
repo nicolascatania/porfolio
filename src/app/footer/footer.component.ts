a08c8543038c6b7e0d0a0757403e3045d90c2897
nicolascatania <nicolas20032401@gmail.com>
2025-02-07 15:53:37 -0300

    add translation for footer and about me components, removed scroll-mt-15 property from all components that had it, suspecting causing bad behaviour on the navbar, because it is no more sticky after passing the hero component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
